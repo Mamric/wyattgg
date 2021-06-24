37586fd919f02523a45c5bd34be0dd548a43c66a
Nick Miller Desktop <nmiller3@go.pasadena.edu>
2021-06-23 18:29:40 -0700

    fixed spacing issue on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main className={""}>
         <h1 className={styles.title}>
           Welcome to <a href="https://www.twitch.tv/scpWyatt">Wyatt.gg!</a>
         </h1>
