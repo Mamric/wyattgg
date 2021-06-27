3f78ef0e1d451d6ee683f33e9d72f3e0a0df8da3
Nick Miller Desktop <nmiller3@go.pasadena.edu>
2021-06-26 19:31:26 -0700

    fixed width issues for twitch component

--- components/Twitch.component.tsx
+++ components/Twitch.component.tsx
@@ -2,23 +2,22 @@ export const Twitch = () => {
   const URLPATH =
     process.env.NODE_ENV === "development" ? "localhost:3000" : "www.wyatt.gg";
   return (
-    <div>
-      <div className="grid justify-center my-6">
-        <div className="h-96 w-screen md:px-36">
+    <div className="grid justify-center">
+      <div className="my-6 max-w-5xl" style={{ width: "97vw" }}>
+        <div className="">
           <iframe
             src={`https://player.twitch.tv/?channel=scpwyatt&parent=${URLPATH}`}
-            frameBorder="0"
             allowFullScreen={true}
             scrolling="no"
-            height="100%"
+            height="400rem"
             width="100%"
           ></iframe>
         </div>
-        <div className="h-96 w-screen md:px-36">
+        <div className="">
           <iframe
             id="chat_embed"
             src={`https://www.twitch.tv/embed/scpwyatt/chat?parent=${URLPATH}&darkpopout`}
-            height="100%"
+            height="500rem"
             width="100%"
           ></iframe>
         </div>
