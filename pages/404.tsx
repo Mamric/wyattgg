c25d79a8ab4daa01ed31754567d1341bb1e797d0
Nick Miller Desktop <nmiller3@go.pasadena.edu>
2022-03-14 11:11:57 -0700

    fixed 404 page not included in commit

---
+++ pages/404.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+const notFoundPage = () => {
+  return (
+    <div className="pt-5">
+      <div className="flex justify-center">
+        <Image
+          className="object-cover"
+          src="/../public/pepe-hands.png"
+          alt={"404 image"}
+          width={200}
+          height={200}
+          priority
+        />
+      </div>
+      <div className="text-center w-full text-gray-100 py-3 text-3xl">
+        404 | Page not found.
+      </div>
+    </div>
+  );
+};
+
+export default notFoundPage;
