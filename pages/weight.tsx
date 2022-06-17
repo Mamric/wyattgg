60b11a785589c50738900cfe1ad9a2f0518af9e8
Nicolas Miller <nmiller3@go.pasadena.edu>
2022-06-16 18:25:32 -0700

    added title to weight page meta tag

--- pages/weight.tsx
+++ pages/weight.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head"
+
 const weightPage = () => {
     interface entry {
         date: string,
@@ -12,29 +14,36 @@ const weightPage = () => {
     ]
 
     return (
-        <div className="text-center text-white my-8">
-            <div className="text-5xl">My Weight</div>
-            <div className="text-1xl italic mt-2">A Gamer&apos;s Journey</div>
-            <div className="my-4 text-center md:text-left flex flex-wrap justify-evenly">
-                {entries.map((entry, i) => (
-                    <div key={i} className="my-5 flex-none w-7/12">
-                        <div className="text-3xl font-bold">Entry {i + 1}:</div>
-                        <div className="">
-                            {new Date(entry.date).toLocaleString('en-us', { weekday: 'long' })}
-                        </div>
-                        <div className="text-xs">
-                            {entry.date}
-                        </div>
-                        <div className="mt-2">Weight: {" "}
-                            <div className="text-xl font-bold inline-block">
-                                {entry.weight}
+        <div>
+            <Head>
+                <title>Weight | Wyatt.gg</title>
+                <meta name="description" content="Wyatt's Website" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="text-center text-white my-8">
+                <div className="text-5xl">My Weight</div>
+                <div className="text-1xl italic mt-2">A Gamer&apos;s Journey</div>
+                <div className="my-4 text-center md:text-left flex flex-wrap justify-evenly">
+                    {entries.map((entry, i) => (
+                        <div key={i} className="my-5 flex-none w-7/12">
+                            <div className="text-3xl font-bold">Entry {i + 1}:</div>
+                            <div className="">
+                                {new Date(entry.date).toLocaleString('en-us', { weekday: 'long' })}
+                            </div>
+                            <div className="text-xs">
+                                {entry.date}
+                            </div>
+                            <div className="mt-2">Weight: {" "}
+                                <div className="text-xl font-bold inline-block">
+                                    {entry.weight}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
+            )
         </div>
-    )
 }
 
 export default weightPage
