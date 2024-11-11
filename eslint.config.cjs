e9bf6ac8d075099987baa376fc67b8a820280ae1
Nick Miller Desktop <nmiller3@go.pasadena.edu>
2024-11-11 10:50:46 -0800

    updated eslint config file again

---
+++ eslint.config.cjs
@@ -0,0 +1,44 @@
+const js = require("@eslint/js");
+const nextPlugin = require("@next/eslint-plugin-next");
+const reactHooks = require("eslint-plugin-react-hooks");
+const typescript = require("@typescript-eslint/eslint-plugin");
+const typescriptParser = require("@typescript-eslint/parser");
+
+module.exports = [
+    {
+        ignores: ["node_modules/**", ".next/**", "dist/**"],
+    },
+    js.configs.recommended,
+    {
+        files: ["**/*.{js,jsx,ts,tsx}"],
+        plugins: {
+            "@next/next": nextPlugin,
+            "react-hooks": reactHooks,
+            "@typescript-eslint": typescript,
+        },
+        languageOptions: {
+            parser: typescriptParser,
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module",
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+            globals: {
+                React: "readonly",
+                JSX: "readonly",
+                document: "readonly",
+                window: "readonly",
+                URL: "readonly",
+                Blob: "readonly",
+            },
+        },
+        rules: {
+            "react-hooks/rules-of-hooks": "error",
+            "react-hooks/exhaustive-deps": "warn",
+            "@next/next/no-html-link-for-pages": "error",
+            "@typescript-eslint/no-unused-vars": "warn",
+        },
+    },
+];
